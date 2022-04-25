df1347e4359069993ba8ab3f334f2bddd6b4c23e
Elena Kane <elena_kane@hotmail.com>
2022-04-25 14:43:34 +0200

    stitching the images

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 
+from matplotlib import image
+import numpy as np 
+import cv2
+import glob
+import imutils
+
+image_paths = glob.glob('images/*.jpeg')
+images = []
+
+#storing each image path
+for i in image_paths:
+    img = cv2.imread(i)
+    images.append(img)
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+
+#stiching the images
+imageStitcher = cv2.Stitcher_create()
+error, stitched_img = imageStitcher.stitch(images)
+
+if not error:
+    cv2.imwrite("stitchedOutput.png", stitched_img)
+    cv2.imshow("stitched image", stitched_img)
+    cv2.waitKey(0)
+
+
